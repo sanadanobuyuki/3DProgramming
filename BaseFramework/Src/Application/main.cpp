5f6ebe78a2afe0eccd233b64e55de040cc9e89fb
sanadanobuyuki <kd1363432@st.kobedenshi.ac.jp>
2024-04-17 11:42:58 +0900

    更新テスト　test変数追加　
    後ほど消す

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,8 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	test = 10;//消す
+
 	return true;
 }
 
